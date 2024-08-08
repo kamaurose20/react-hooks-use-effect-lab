2d0b4d0e11135c4d5575dce4c91fc1971a3058a3
rosekamau <kamaurose236@gmail.com>
2024-08-08 14:08:09 +0300

    react-hooks-use-effect-lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,37 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const timeout = setTimeout(()=>{setTimeRemaining(count=>count-1)
+    },1000);
+
+    return function clearTimer(){
+      clearTimeout(timeout)
+    }
+
+  },[timeRemaining])
+
+  if(timeRemaining<1){
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
+  // Handles the button click, sets the remaining time and activates the callback
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
+    setTimeRemaining(10); // Reset the timer back to 10 seconds
+    onAnswered(isCorrect); // Callback function to send the answer status
   }
 
-  const { id, prompt, answers, correctIndex } = question;
-
+  const { id, prompt, answers, correctIndex } = question; // Destructures props for easier access
+  // Renders the question, possible answers, and the timer
   return (
     <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
+      <h1>Question {id}</h1> {/* Displays the question's identifier*/}
+      <h3>{prompt}</h3> {/* Displays the question's prompt */}
+      {answers.map((answer, index) => { // Maps through the answers array to render buttons
+        const isCorrect = index === correctIndex; // Checks if the current answer is correct
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
@@ -29,4 +43,4 @@ function Question({ question, onAnswered }) {
   );
 }
 
-export default Question;
+export default Question; // Exports the component for use in other parts of the application
